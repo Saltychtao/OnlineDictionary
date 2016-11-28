e624f8f6a61c52ecfe0b0015542ab06b40525f6b
lhh201107 <lhh201107@live.com>
2016-11-28 23:58:55 +0800

    fix sign in add sign up add praise

--- src/DictionaryServer/Server.java
+++ src/DictionaryServer/Server.java
@@ -4,13 +4,41 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 import java.io.IOException;
 public class Server {
-    static String[][] users= new String[16][2];
+    static String[][] users= new String[16][3];
     static int UserCount=0;
+
+    private static void AddUser(String fileName, String content) {
+        try {
+            FileWriter writer = new FileWriter(fileName, true);
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void praise(int Dictype,String word)
+    {
+    	try {
+            FileWriter writer = new FileWriter("words.txt", true);
+            if(Dictype==1)
+            writer.write(word+"	1	0	0\r\n");
+            else if(Dictype==2)
+                writer.write(word+"	0	1	0\r\n");
+            else if(Dictype==3)
+                writer.write(word+"	0	0	1\r\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     private static void LoadUserInformation()
     {
     	    File sourceFile = new File("users.txt");
@@ -35,6 +63,7 @@ public class Server {
 				 array = s.split("	");	
 				 users[UserCount][0]=array[0];
 			     users[UserCount][1]=array[1];
+			     users[UserCount][2]=array[2];
 			     UserCount++;
 			}
 			input.close();
@@ -43,16 +72,18 @@ public class Server {
     {
     	for(int temp=0;temp<UserCount;temp++)
     	{
-    		if(UserName==users[temp][0]&&password==users[temp][1])
+    		if(UserName.equals(users[temp][0])&&password.equals(users[temp][1]))
     			return true;
     	}
-		return false;
+    	return false;
+		
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  System.out.println("Loading user information");
-		  System.out.println("Server is staring!");
 		  LoadUserInformation();
+		  
+		  System.out.println("Server is staring!");
 		  int i = 0;
 		  try   
 		  {    
@@ -66,11 +97,15 @@ public class Server {
 		         .getOutputStream());   
 		      String keyword = new String(sinputstream.readUTF());   
 		      System.out.print("get word");
-		      String[] strs = keyword.split("##");
-		      if (strs[1].equals("1"))
+		      String[] strs = keyword.split("#");
+		      if(strs[1].equals("0"))
+		         {
+		    	   AddUser("users.txt",strs[2]+"	"+strs[3]+"	"+strs[4]+"\r\n");	
+		         }
+		      else if (strs[1].equals("1"))
 		        {
 		    	  
-		    	  if(CheckLogin(strs[3],strs[5]))
+		    	  if(CheckLogin(strs[2],strs[3]))
 		        	soutputstream.writeUTF("#1#login success");
 		    	  else
 		    	    soutputstream.writeUTF("#1#user name or password error"); 
@@ -78,16 +113,34 @@ public class Server {
 		        }
 		      else if (strs[1].equals("2"))
 		        {	
-		    	    String result1=BaiduMain.BaiduTranslate(strs[3]);
-				    String result2=YoudaoMain.YoudaoTranslate(strs[3]);
-				    String result3 = "";
-		        	soutputstream.writeUTF("#2#1"+result1);
-		        	soutputstream.writeUTF("#2#2"+result2);
-		        	soutputstream.writeUTF("#2#3"+result3);
+		    	    if(strs[2].equals("1"))
+		    	    {
+		    	    	soutputstream.writeUTF("#2#1"+BaiduMain.BaiduTranslate(strs[3]));
+		    	    }
+		    	    else if(strs[2].equals("2"))
+		    	    {
+		    	    	soutputstream.writeUTF("#2#2"+YoudaoMain.YoudaoTranslate(strs[3]));
+		    	    }
+		    	    else
+		    	    { 
+		        	soutputstream.writeUTF("#2#3"+"");
+		    	    }
 		        }
-		      else if (strs[1].equals("3"))
+		      else if (strs[0].equals("3"))
 		        {	
-		        	soutputstream.writeUTF("iii");
+		    	    if(strs[2].equals("1"))
+		    	    {
+		    	    	praise(1,strs[3]);
+		    	    }
+		    	    else if(strs[2].equals("2"))
+		    	    {
+		    	    	praise(2,strs[3]);
+		    	    }
+		    	    else
+		    	    { 
+		    	    	praise(3,strs[3]);
+		    	    }
+		        	soutputstream.writeUTF("#3#praise successfully");
 		        }
 		      soutputstream.close();   
 		      soutputstream.close();   
